b1d207e339e6695b99dc04dbd9971b6ec44f21f0
Berk Ozer <galipberkozer@gmail.com>
2020-03-30 15:10:49 -0400

    Reader method for id

--- Player.rb
+++ Player.rb
@@ -1,12 +1,12 @@
 class Player
-  attr_reader :current_lives
+  attr_reader :current_lives, :id
 
   def initialize(id)
     @id = id
     @current_lives = 3
   end
 
-  # Game calls this when player answer question incorrectly
+  # Game calls this when player answers question incorrectly
   def lose_life
     @current_lives -= 1
   end

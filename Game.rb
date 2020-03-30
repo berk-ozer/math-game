e4568dd2354d95c9b45b7759ebbaabbeb76457cd
Berk Ozer <galipberkozer@gmail.com>
2020-03-30 11:06:17 -0400

    Give instructions in start_game

---
+++ Game.rb
@@ -0,0 +1,16 @@
+class Game
+  def initialize
+    
+  end
+
+  def start_game
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "Welcome to the Math Game!"
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "Each player has 3 lives, and you lose a life\nwhen you answer a question incorrectly."
+    puts "----"
+    puts "When either one of the players runs out of\nlives, the game ends."
+    puts "----"
+    puts "If you're ready to start, type 'y' in the prompt\n"
+  end
+end

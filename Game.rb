183a7d834454ac66ff30c06950c008d258589bd4
Berk Ozer <galipberkozer@gmail.com>
2020-03-30 14:06:08 -0400

    Player loses life if they answered incorrectly

--- Game.rb
+++ Game.rb
@@ -3,7 +3,8 @@ class Game
   def initialize
     @player_1 = Player.new(1)
     @player_2 = Player.new(2)
-    @current_player = 1
+    @players = [@player_1, @player_2]
+    @current_player = 0
   end
 
   # Presents instructions before starting game
@@ -32,18 +33,25 @@ class Game
         answer = gets.chomp
       end
     end
-    puts "\n~~~~~~~~~~ The Game starts now! ~~~~~~~~~~"
+    puts "\n~~~~~~~~~~ GAME STARTS NOW! ~~~~~~~~~~"
     next_turn
   end
 
   # Starts the next turn, asking the current player a question
   def next_turn
-    turn = Turn.new(1)
-    turn.ask_question
+    turn = Turn.new(@current_player)
+    player_answer = turn.ask_question
+
+    # Player loses life if they answered incorrectly
+    if (player_answer == false) 
+      @players[@current_player].lose_life
+    end
+
   end
 
   # Switches to the other player when a turn ends
   def switch_player
     @current_player = @current_player == 1 ? 2 : 1
   end
+
 end
